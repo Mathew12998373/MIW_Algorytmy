cf4356bce67b7d5888c27a0d4582e73124bd01db
Mathew12998373 <doGita1@wp.pl>
2025-03-19 15:18:46 +0100

    Add new files

---
+++ ALGORYTM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	static string znalezienie_parametrow(double x1, double x2)
+	{
+		double F;
+		return F = Math.sin(x1 * 0.05) + Math.sin(x2 * 0.05) + 0.4 * Math.sin(x1 * 0.15) * Math.sin(x2 * 0.15);
+	}
+
+	static void Main()
+	{
+		int ilosc_bitow = 3;
+		int liczba_iteracji = 20;
+		int liczba_osobnikow = 9;
+		int rozmiar_turnieju = 2;
+
+	}
+}
