004641428dd8b574aec577480ba2443643c9adde
Mathew12998373 <doGita1@wp.pl>
2025-04-07 12:29:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -37,31 +37,27 @@ class Class
         Random rnd = new Random();
         int L_B_CH = LBnP * liczba_parametrow;
 
-        for (int i=0; i<liczba_osobnikow; i++)
+        for (int i = 0; i < liczba_osobnikow; i++)
         {
-            string osobnik = " ";
-            for (int j=0; j<L_B_CH; j++)
+            string osobnik = "";
+            for (int j = 0; j < L_B_CH; j++)
             {
                 int losowanie = rnd.Next(0, 2);
                 if (losowanie == 1)
                 {
                     osobnik += "1";
                 }
-                else if(losowanie == 0)
+                else if (losowanie == 0)
                 {
                     osobnik += "0";
                 }
             }
             Pula.Add(osobnik);
         }
-        foreach(var osobnik in Pula)
-        {
-            Console.WriteLine("osobnik = {0}", osobnik);
-        }
         return Pula;
 
     }
-    static List<(string,double,double)> Dekodowanie (Dictionary<string,double> Tabela, List<string> Pula, int LBnP)
+    static List<(string, double, double)> Dekodowanie(Dictionary<string, double> Tabela, List<string> Pula, int LBnP)
     {
         List<(string, string, string)> PulaX = new List<(string, string, string)>();
         List<(string, double, double)> Pula_wartosci = new List<(string, double, double)>();
@@ -71,13 +67,13 @@ class Class
         double X1;
         double X2;
 
-        foreach(var osobnik in Pula)
+        foreach (var osobnik in Pula)
         {
             x1 = osobnik.Substring(0, LBnP);
             x2 = osobnik.Substring(LBnP);
             PulaX.Add((osobnik, x1, x2));
         }
-        foreach(var osobnik in PulaX)
+        foreach (var osobnik in PulaX)
         {
             KodBin_osobnika = osobnik.Item1;
             X1 = Tabela[osobnik.Item2];
@@ -86,18 +82,103 @@ class Class
         }
         return Pula_wartosci;
     }
-    static List<(string,double)> ocena_osobnika (List<(string,double,double)> Pula_zdekodowana)
+    static List<(string, double)> ocena_osobnika(List<(string, double, double)> Pula_zdekodowana)
     {
         List<(string, double)> osobnicy = new List<(string, double)>();
-        foreach(var krotka in Pula_zdekodowana)
+        foreach (var krotka in Pula_zdekodowana)
         {
             osobnicy.Add((krotka.Item1, Funkcja_przystosowania(krotka.Item2, krotka.Item3)));
         }
-        foreach (var i in osobnicy)
+        return osobnicy;
+    }
+    static (string, double) najlepszy_z_puli(List<(string, double)> Pula)
+    {
+        (string, double) najlepszy = Pula[0];
+        foreach (var krotka in Pula)
+        {
+            if (krotka.Item2 > najlepszy.Item2)
+            {
+                najlepszy = krotka;
+            }
+        }
+        Console.WriteLine(najlepszy);
+        return najlepszy;
+    }
+    static List<(string, double)> Turniej(List<(string, double)> Pula, int liczba_osobnikow)
+    {
+        List<(string, double)> nowa_pula = new List<(string, double)>();
+        Random rnd = new Random();
+
+        for (int i = 0; i < liczba_osobnikow - 1; i++)
+        {
+            int osobnik1 = rnd.Next(0, liczba_osobnikow);
+            int osobnik2 = rnd.Next(0, liczba_osobnikow);
+            if (Pula[osobnik1].Item2 >= Pula[osobnik2].Item2)
+            {
+                nowa_pula.Add(Pula[osobnik1]);
+            }
+            else if (Pula[osobnik1].Item2 < Pula[osobnik2].Item2)
+            {
+                nowa_pula.Add(Pula[osobnik2]);
+            }
+        }
+        return nowa_pula;
+    }
+    static List<string> Mutator(List<(string, double)> Pula)
+    {
+        List<string> zmutowane = new List<string>();
+        Random rnd = new Random();
+
+        foreach (var osobnik in Pula)
+        {
+            int b = rnd.Next(0, osobnik.Item1.Length);
+            string początek = osobnik.Item1.Substring(0, b);
+            string koniec = osobnik.Item1.Substring(b + 1);
+            char nowy_Bit = new char();
+            if (osobnik.Item1[b] == '1')
+            {
+                nowy_Bit = '0';
+            }
+            else if (osobnik.Item1[b] == '0')
+            {
+                nowy_Bit = '1';
+            }
+            string zmutowanyOsobnik = początek + nowy_Bit + koniec;
+            zmutowane.Add(zmutowanyOsobnik);
+        }
+        return zmutowane;
+    }
+    static double srednia(List<(string, double)> Pula, int liczba_osobnikow)
+    {
+        double średnia = 0;
+        double suma = 0;
+        for (int i = 0; i < liczba_osobnikow - 1; i++)
+        {
+            suma += Pula[i].Item2;
+        }
+        średnia = Math.Round(suma / liczba_osobnikow, 2);
+        return średnia;
+    }
+    static void wyswietl_osobnikow(List<string> Pula)
+    {
+        foreach (var i in Pula)
         {
             Console.WriteLine("osobnik: {0}", i);
         }
-        return osobnicy;
+    }
+    static void wyswietl_osobnikow(List<(string, double, double)> Pula_osobnikow)
+    {
+        foreach (var krotka in Pula_osobnikow)
+        {
+            Console.WriteLine("{0}  {1}\t{2}", krotka.Item1, krotka.Item2, krotka.Item3);
+        }
+    }
+    static void wyswietl_osobnikow(List<(string, double)> osobnicy)
+    {
+        foreach (var krotka in osobnicy)
+        {
+            Console.WriteLine("osobnik: {0}, ocena: {1}", krotka.Item1, krotka.Item2);
+        }
     }
     static void Main(string[] args)
     {
@@ -110,7 +191,31 @@ class Class
 
         Dictionary<string, double> tablicaKodowania = Tablica_kodowania(Min, Max, LBnP);
         List<string> Pula = Pula_osobnikow(liczba_osobnikow, liczba_parametrow, LBnP);
+        Console.WriteLine("Pula osobników\t");
+        wyswietl_osobnikow(Pula);
+        Console.WriteLine("\t");
         List<(string, double, double)> Pula_zdekodowana = Dekodowanie(tablicaKodowania, Pula, LBnP);
+        Console.WriteLine("Pula osobników zdekodowananych\t");
+        wyswietl_osobnikow(Pula_zdekodowana);
+        Console.WriteLine("\t");
         List<(string, double)> Pula_oceniona = ocena_osobnika(Pula_zdekodowana);
+        Console.WriteLine("Pula oceniona\t");
+        wyswietl_osobnikow(Pula_oceniona);
+        Console.WriteLine("\t");
+        (string, double) najlepszy = najlepszy_z_puli(Pula_oceniona);
+        Console.WriteLine("Najlepiej dostosowany: {0}, {1} \t średnia dostosowania: {2}\n", najlepszy.Item1, najlepszy.Item2, srednia(Pula_oceniona, liczba_osobnikow));
+
+        for (int i = 0; i < liczba_iteracji; i++)
+        {
+            List<(string, double)> nowa_Pula = Turniej(Pula_oceniona, liczba_osobnikow);
+            List<string> nowa_po_mutacji = Mutator(nowa_Pula);
+            List<(string, double, double)> nowa_Pula_dekodowanie = Dekodowanie(tablicaKodowania, nowa_po_mutacji, LBnP);
+            List<(string, double)> nowa_Pula_oceniona = ocena_osobnika(nowa_Pula_dekodowanie);
+            nowa_Pula_oceniona.Add(najlepszy);
+            wyswietl_osobnikow(nowa_Pula_oceniona);
+            najlepszy = najlepszy_z_puli(nowa_Pula_oceniona);
+            Console.WriteLine("Najlepiej dostosowany: {0}, {1} \t średnia dostosowania: {2}\n", najlepszy.Item1, najlepszy.Item2, srednia(nowa_Pula_oceniona, liczba_osobnikow));
+            Pula_oceniona = nowa_Pula_oceniona;
+        }
     }
 }
