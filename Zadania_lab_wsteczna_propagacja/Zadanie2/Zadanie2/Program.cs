a04cb8f2324536e4a2a5db9200d7d15c7a8645c5
Mathew12998373 <doGita1@wp.pl>
2025-05-06 12:22:16 +0200

    Add files via upload

---
+++ Zadania_lab_wsteczna_propagacja/Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Zadanie2
+{
+    static List<(int, int)> LiczbaNeuronow()
+    {
+        List<(int, int)> Liczba_neuronów = new List<(int, int)>
+        {
+            (2,2),(2,2),(2,2)
+        };
+        foreach (var i in Liczba_neuronów)
+        {
+            Console.WriteLine(i);
+        }
+        return Liczba_neuronów;
+    }
+    
+
+    static void Main(string[] args)
+    {
+        int Beta = 1;
+        double Współczynnik = 0.3;
+        int liczbaEpok = 50000;
+        List<(int, int, int, int)> probki = new List<(int, int, int, int)>
+        {
+            (0,0,0,1),
+            (0,1,1,0),
+            (1,0,1,0),
+            (1,1,0,0)
+        };
+
+        var liczbaNeuronow = LiczbaNeuronow();
+    }
+}
