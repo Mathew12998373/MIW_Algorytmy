650f3f768f323784523c23e7a32d6b65c20ee76f
Mathew12998373 <doGita1@wp.pl>
2025-05-10 21:32:39 +0200

    Add files via upload

---
+++ Zadania_lab_wsteczna_propagacja/GUI_zadanie1/GUI_zadanie1/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace SiecNeuronowaGUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnWyswietl;
+        private System.Windows.Forms.TextBox outputBox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            btnWyswietl = new Button();
+            outputBox = new TextBox();
+            SuspendLayout();
+            btnWyswietl.BackColor = Color.AliceBlue;
+            btnWyswietl.Font = new Font("Century", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnWyswietl.Location = new Point(131, 34);
+            btnWyswietl.Margin = new Padding(4, 5, 4, 5);
+            btnWyswietl.Name = "btnWyswietl";
+            btnWyswietl.Size = new Size(372, 47);
+            btnWyswietl.TabIndex = 0;
+            btnWyswietl.Text = "Kliknij, aby wyświetlić wyniki";
+            btnWyswietl.UseVisualStyleBackColor = false;
+            btnWyswietl.Click += btnWyswietl_Click;
+
+            outputBox.BackColor = Color.FromArgb(224, 224, 224);
+            outputBox.Font = new Font("Franklin Gothic Book", 10F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            outputBox.Location = new Point(84, 111);
+            outputBox.Margin = new Padding(4, 5, 4, 5);
+            outputBox.Multiline = true;
+            outputBox.Name = "outputBox";
+            outputBox.ReadOnly = true;
+            outputBox.Size = new Size(450, 300);
+            outputBox.TabIndex = 1;
+
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Azure;
+            ClientSize = new Size(628, 444);
+            Controls.Add(btnWyswietl);
+            Controls.Add(outputBox);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Sieć Neuronowa GUI";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ Zadania_lab_wsteczna_propagacja/GUI_zadanie1/GUI_zadanie1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SiecNeuronowaGUI
+{
+    public partial class Form1 : Form
+    {
+        private (List<List<List<double>>> Wagi, List<List<double>> Bias) siec;
+        private List<(int x1, int x2, int y)> probki;
+        private int beta = 1;
+        private double wspolczynnik = 0.3;
+        private int liczbaEpok = 50000;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            probki = new List<(int, int, int)>
+            {
+                (0,0,0),
+                (0,1,1),
+                (1,0,1),
+                (1,1,0)
+            };
+
+            var liczbaNeuronow = new List<(int, int)>
+            {
+                (2, 2),
+                (1, 2)
+            };
+
+            siec = GenerowanieWag(liczbaNeuronow);
+            Sieci(probki, siec, beta, wspolczynnik, liczbaEpok);
+        }
+
+        private void btnWyswietl_Click(object sender, EventArgs e)
+        {
+            string wyniki = "";
+
+            foreach (var (x1, x2, y) in probki) 
+            {
+                var output = Propagacja(siec, new List<double> { x1, x2 }, beta);
+                wyniki += $"Wejście: {x1}, {x2} pożądana wartość wyjściowa: {y} wyjście: {output[0]:F2}\n";
+            }
+
+            outputBox.Text = wyniki;
+        }
+        
+
+
+        private double Funkcja(double x, int beta)
+        {
+            return 1.0 / (1.0 + Math.Exp(-beta * x));
+        }
+
+        private (List<List<List<double>>> Wagi, List<List<double>> Bias) GenerowanieWag(List<(int neurony, int wejscia)> liczbaNeuronow)
+        {
+            List<List<List<double>>> Wagi = new List<List<List<double>>>();
+            List<List<double>> Bias = new List<List<double>>();
+            var rnd = new Random();
+
+            foreach (var (neurony, wejścia) in liczbaNeuronow)
+            {
+                List<List<double>> wagiWarstwy = new List<List<double>>();
+                List<double> biasWarstwy = new List<double>();
+
+                for (int i = 0; i < neurony; i++)
+                {
+                    List<double> wagiNeuronu = new List<double>();
+                    for (int j = 0; j < wejścia; j++)
+                    {
+                        wagiNeuronu.Add(rnd.Next(-5, 6));
+                    }
+                    wagiWarstwy.Add(wagiNeuronu);
+                    biasWarstwy.Add(rnd.Next(-5, 6));
+                }
+
+                Wagi.Add(wagiWarstwy);
+                Bias.Add(biasWarstwy);
+            }
+
+            return (Wagi, Bias);
+        }
+
+        private List<double> Propagacja((List<List<List<double>>> Wagi, List<List<double>> Bias) GenerowanieWag, List<double> wejscia, int beta)
+        {
+            List<double> wyjscia = new List<double>(wejscia);
+            for (int l = 0; l < GenerowanieWag.Wagi.Count; l++)
+            {
+                List<double> noweWyjscia = new List<double>();
+                for (int n = 0; n < GenerowanieWag.Wagi[l].Count; n++)
+                {
+                    double suma = GenerowanieWag.Bias[l][n];
+                    for (int i = 0; i < wyjscia.Count; i++)
+                    {
+                        suma += wyjscia[i] * GenerowanieWag.Wagi[l][n][i];
+                    }
+                    noweWyjscia.Add(Funkcja(suma, beta));
+                }
+                wyjscia = noweWyjscia;
+            }
+            return wyjscia;
+        }
+
+        private void Sieci(List<(int x1, int x2, int y)> probki, (List<List<List<double>>> Wagi, List<List<double>> Bias) siec, int beta, double wspolczynnik, int liczbaEpok)
+        {
+            int Liczba_warstw = siec.Wagi.Count;
+
+            for (int epoka = 0; epoka < liczbaEpok; epoka++)
+            {
+                double sumarycznyBlad = 0.0;
+
+                foreach (var (x1, x2, x3) in probki)
+                {
+                    List<double> wejscia = new List<double> { x1, x2 };
+                    List<List<double>> wyjsciaWarstw = new List<List<double>>();
+                    List<double> aktualne = new List<double>(wejscia);
+                    for (int l = 0; l < Liczba_warstw; l++)
+                    {
+                        List<double> Wyjscie_L = new List<double>();
+                        for (int n = 0; n < siec.Wagi[l].Count; n++)
+                        {
+                            double suma = siec.Bias[l][n];
+                            for (int i = 0; i < aktualne.Count; i++)
+                            {
+                                suma += aktualne[i] * siec.Wagi[l][n][i];
+                            }
+                            Wyjscie_L.Add(Funkcja(suma, beta));
+                        }
+                        wyjsciaWarstw.Add(Wyjscie_L);
+                        aktualne = Wyjscie_L;
+                    }
+                    double y_Sieci = aktualne[0];
+                    double blad = x3 - y_Sieci;
+                    sumarycznyBlad += Math.Abs(blad);
+                    var D = new List<double>[Liczba_warstw];
+                    for (int i = 0; i < Liczba_warstw; i++)
+                        D[i] = new List<double>(new double[siec.Wagi[i].Count]);
+
+                    int ostatnia = Liczba_warstw - 1;
+                    for (int j = 0; j < siec.Wagi[ostatnia].Count; j++)
+                    {
+                        double o = wyjsciaWarstw[ostatnia][j];
+                        D[ostatnia][j] = blad * beta * o * (1 - o);
+                    }
+
+                    for (int l = Liczba_warstw - 2; l >= 0; l--)
+                    {
+                        for (int i = 0; i < siec.Wagi[l].Count; i++)
+                        {
+                            double Suma = 0.0;
+                            for (int k = 0; k < siec.Wagi[l + 1].Count; k++)
+                            {
+                                Suma += siec.Wagi[l + 1][k][i] * D[l + 1][k];
+                            }
+
+                            double o = wyjsciaWarstw[l][i];
+                            D[l][i] = Suma * beta * o * (1 - o);
+                        }
+                    }
+                    for (int l = 0; l < Liczba_warstw; l++)
+                    {
+                        List<double> poprzednie_wyjscie;
+                        if (l == 0)
+                            poprzednie_wyjscie = wejscia;
+                        else
+                            poprzednie_wyjscie = wyjsciaWarstw[l - 1];
+
+                        for (int n = 0; n < siec.Wagi[l].Count; n++)
+                        {
+                            for (int i = 0; i < poprzednie_wyjscie.Count; i++)
+                            {
+                                siec.Wagi[l][n][i] += wspolczynnik * D[l][n] * poprzednie_wyjscie[i];
+                            }
+                            siec.Bias[l][n] += wspolczynnik * D[l][n];
+                        }
+                    }
+                }
+                if (sumarycznyBlad < 0.3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
